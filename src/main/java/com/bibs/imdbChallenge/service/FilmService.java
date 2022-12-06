ba7acee892cf77792a4bff15829262232f48af0b
Brian <brian.fly0623@gmail.com>
2022-12-06 18:04:26 -0300

    :sparkles: adicionando a anotação @AutoWired para injeção de dependências

--- src/main/java/com/bibs/imdbChallenge/service/FilmService.java
+++ src/main/java/com/bibs/imdbChallenge/service/FilmService.java
@@ -1,4 +1,13 @@
 package com.bibs.imdbChallenge.service;
 
+import com.bibs.imdbChallenge.client.FilmClientOmdbFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FilmService {
+
+    // não é necessário criar um novo objeto (com new) pq o spring já permite a injeção de tipos
+    @Autowired // injeta dependências
+    private FilmClientOmdbFeign clientOmdbFeign;
 }
