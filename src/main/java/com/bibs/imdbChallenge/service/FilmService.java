79b918f6d90b29fe0850d663f547c51222fb74b0
Mykyta <levinmykyta@gmail.com>
2022-12-07 16:04:01 -0300

    :sparkles: adding method and connection to application.properties to Service and creating OMDB apiKey on application.properties

--- src/main/java/com/bibs/imdbChallenge/service/FilmService.java
+++ src/main/java/com/bibs/imdbChallenge/service/FilmService.java
@@ -1,13 +1,23 @@
 package com.bibs.imdbChallenge.service;
 
 import com.bibs.imdbChallenge.client.FilmClientOmdbFeign;
+import com.bibs.imdbChallenge.vo.FilmOmdb;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FilmService {
 
+    @Value("${imdb.apiKey}") // pega o que ta no applications.properties
+    private String apiKey;
+
     // não é necessário criar um novo objeto (com new) pq o spring já permite a injeção de tipos
     @Autowired // injeta dependências
-    private FilmClientOmdbFeign clientOmdbFeign;
+    private FilmClientOmdbFeign filmClientFeign;
+
+    // método
+    public FilmOmdb getFilm(String theme) {
+        return filmClientFeign.getFilm(theme, apiKey);
+    }
 }
