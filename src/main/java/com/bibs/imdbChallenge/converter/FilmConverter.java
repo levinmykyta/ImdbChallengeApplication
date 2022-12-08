935c673300858abd4965f3002bdd8552c24812f4
Mykyta <levinmykyta@gmail.com>
2022-12-08 17:11:39 -0300

    :sparkles: creating converter to convert film to filmDTO using @Component annotation

--- src/main/java/com/bibs/imdbChallenge/converter/FilmConverter.java
+++ src/main/java/com/bibs/imdbChallenge/converter/FilmConverter.java
@@ -1,2 +1,16 @@
-package com.bibs.imdbChallenge.converter;public class FilmConverter {
+package com.bibs.imdbChallenge.converter;
+
+import com.bibs.imdbChallenge.dto.FilmDTO;
+import com.bibs.imdbChallenge.model.Film;
+import org.springframework.stereotype.Component;
+
+@Component // componente do spring, usado para injetar dependências
+public class FilmConverter {
+
+    public Film convertToFilm(FilmDTO filmDTO) {
+        Film film = new Film();
+        film.setTitle(filmDTO.getTitle());
+        film.setYear(filmDTO.getYear());
+        return film;
+    }
 }
