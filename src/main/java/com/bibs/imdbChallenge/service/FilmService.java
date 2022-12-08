34e7561f054dcf8f07513c4059f2527575c7d1ef
Mykyta <levinmykyta@gmail.com>
2022-12-08 17:12:46 -0300

    :label: adding filmConverter as a object in FilmService to use method

--- src/main/java/com/bibs/imdbChallenge/service/FilmService.java
+++ src/main/java/com/bibs/imdbChallenge/service/FilmService.java
@@ -1,7 +1,12 @@
 package com.bibs.imdbChallenge.service;
 
 import com.bibs.imdbChallenge.client.FilmClientOmdbFeign;
+import com.bibs.imdbChallenge.converter.FilmConverter;
+import com.bibs.imdbChallenge.dto.FilmDTO;
+import com.bibs.imdbChallenge.model.Film;
+import com.bibs.imdbChallenge.repository.FilmRepository;
 import com.bibs.imdbChallenge.vo.FilmOmdb;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,8 +21,20 @@ public class FilmService {
     @Autowired // injeta dependências
     private FilmClientOmdbFeign filmClientFeign;
 
-    // método
+    @Autowired
+    private FilmConverter filmConverter; // precisamos anotar @Component na classe do Converter para inicializarmos aqui
+
+    @Autowired
+    private FilmRepository filmRepository;
+
+    // métodos
     public FilmOmdb getFilm(String theme) {
         return filmClientFeign.getFilm(theme, apiKey);
     }
+
+    public Film save(FilmDTO filmDTO) {
+        Film film = filmConverter.convertToFilm(filmDTO);
+        return filmRepository.save(film);
+    }
+
 }
