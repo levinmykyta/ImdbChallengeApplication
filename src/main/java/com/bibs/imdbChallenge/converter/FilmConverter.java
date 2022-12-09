b22a60526ebead38deeb2acc77363ba70801a3cd
Mykyta <levinmykyta@gmail.com>
2022-12-09 16:31:38 -0300

    :sparkles: creating converting method from Film to FilmVO, preparing it to be delivered to the client

--- src/main/java/com/bibs/imdbChallenge/converter/FilmConverter.java
+++ src/main/java/com/bibs/imdbChallenge/converter/FilmConverter.java
@@ -2,6 +2,7 @@ package com.bibs.imdbChallenge.converter;
 
 import com.bibs.imdbChallenge.dto.FilmDTO;
 import com.bibs.imdbChallenge.model.Film;
+import com.bibs.imdbChallenge.vo.FilmVO;
 import org.springframework.stereotype.Component;
 
 @Component // componente do spring, usado para injetar dependências
@@ -12,5 +13,14 @@ public class FilmConverter {
         film.setTitle(filmDTO.getTitle());
         film.setYear(filmDTO.getYear());
         return film;
+    }
+
+    // faz a conversão do film para o filmVO (também poderia ser feito no FilmService)
+    public FilmVO convertToFilmVO(Film film) {
+        FilmVO filmVO = new FilmVO();
+        filmVO.setId(film.getId());
+        filmVO.setTitle(film.getTitle());
+        filmVO.setYear(film.getYear());
+        return filmVO;
     }
 }
