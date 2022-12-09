8ca53fcf3a9a60ddfef259d1165127cb596f129f
Brian <brian.fly0623@gmail.com>
2022-12-09 16:30:36 -0300

    :sparkles: creating FilmVO class. This class is used to send used and saved and information to the client, after we get them through FilmDTO

--- src/main/java/com/bibs/imdbChallenge/vo/FilmVO.java
+++ src/main/java/com/bibs/imdbChallenge/vo/FilmVO.java
@@ -1,2 +1,37 @@
-package com.bibs.imdbChallenge.vo;public class FilmVO {
+package com.bibs.imdbChallenge.vo;
+
+import org.springframework.hateoas.RepresentationModel;
+
+// usamos essa classe para devolver as informações depois que pegamos ela do cliente (pelo FilmDTO) e salvamos
+public class FilmVO extends RepresentationModel<FilmVO> {
+
+    private Long id;
+    private String title;
+    private String year;
+
+    // GETTERS & SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
 }
